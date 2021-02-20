325a9158c993721eb64739780427b4e7732278fa
Angel Mendez <angelmendezz751@gmail.com>
2021-02-19 22:56:13 -0600

    Agregamos el social media

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
-import {CONTACT_EMAIL, CONTACT_PHONE} from "../../utils/info";
-import {FaPhone} from "react-icons/fa";
+import {CONTACT_EMAIL, CONTACT_PHONE, SOCIAL_MEDIA} from "../../utils/info";
+import {FaPhone, FaFacebookF, FaTwitter, FaInstagram} from "react-icons/fa";
 import {MdEmail} from 'react-icons/md';
-import {RiWhatsappFill} from 'react-icons/ri';
+import {RiWhatsappFill, RiInstagramFill} from 'react-icons/ri';
 
 export default function Footer({}) {
     return (
@@ -38,6 +38,28 @@ export default function Footer({}) {
                         <li className="text-gray-800 flex">
                             <a href={`phone:${CONTACT_PHONE}`} className="flex items-center">
                                 <FaPhone className={"mr-3"}/> {CONTACT_PHONE}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="w-full">
+                    <h5 className="font-medium text-lg">
+                        Social
+                    </h5>
+                    <ul className="w-full space-y-4 mt-3">
+                        <li className="text-gray-800 flex items-center">
+                            <a href={`https://facebook.com/${SOCIAL_MEDIA.facebook}`} className="flex items-center">
+                                <FaFacebookF className={"mr-3"}/> /{SOCIAL_MEDIA.facebook}
+                            </a>
+                        </li>
+                        <li className="text-gray-800 flex">
+                            <a href={`https://instagram.com/${SOCIAL_MEDIA.instagram}`} className="flex items-center">
+                                <RiInstagramFill className={"mr-3"}/> {SOCIAL_MEDIA.instagram}
+                            </a>
+                        </li>
+                        <li className="text-gray-800 flex">
+                            <a href={`https://twitter.com/${SOCIAL_MEDIA.twitter}`} className="flex items-center">
+                                <FaTwitter className={"mr-3"}/> {SOCIAL_MEDIA.twitter}
                             </a>
                         </li>
                     </ul>
