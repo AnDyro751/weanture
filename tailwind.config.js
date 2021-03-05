3f061f9a46920bcf6cce3ccd87aecff716c49d9e
Angel Mendez <angelmendezz751@gmail.com>
2021-03-05 17:44:54 -0600

    Purge content

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    purge: {
+        content: [
+            './pages/**/*.{js,jsx,ts,tsx}',
+            './src/**/*.{js,jsx,ts,tsx}',
+            './public/**/*.{js,jsx,ts,tsx}'
+        ]
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            backgroundColor: (theme) => ({
+                ...theme("colors"),
+                "whatsapp": "#25d366",
+                main: "#c8ff00"
+            })
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
