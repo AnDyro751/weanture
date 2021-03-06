7d5f4368618554cc486fd02aeb996b9921ab45e7
Angel Mendez <angelmendezz751@gmail.com>
2021-03-05 19:12:55 -0600

    ERROR

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import MainLayout from "../src/components/Layouts/main";
 import BannerWelcome from "../src/components/pages/BannerWelcome";
 import dynamic from 'next/dynamic'
-import BannerStack from "../src/components/BannerStack";
-
 const BannerStack = dynamic(() => import('../src/components/BannerStack'));
 const ContactForm = dynamic(() => import('../src/components/ContactForm'));
 const OurExperience = dynamic(() => import('../src/components/OurExperince'));
